43c6ec1124ab9db540dfa8e0eae4cb097dc6f0d3
r1oga <38692952+r1oga@users.noreply.github.com>
2022-10-27 23:22:17 +0200

    🔧let prettier config override eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,20 +1,14 @@
 // no use of eslint-plugin-prettier: it is against prettier philosophy https://prettier.io/docs/en/integrating-with-linters.html#notes
 
 module.exports = {
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier', 'standard'],
+  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'standard', 'prettier'],
   parser: '@typescript-eslint/parser',
   plugins: ['@typescript-eslint', 'jest'],
   root: true,
   rules: {
     "@typescript-eslint/ban-ts-comment": "warn",
-    "comma-dangle": ["error", "always-multiline"],
     "no-console": "warn",
-    "no-useless-constructor": "warn",
-    "space-before-function-paren": ["error", {
-      "anonymous": "never",
-      "asyncArrow": "always",
-      "named": "never"
-    }]
+    "no-useless-constructor": "warn"
   },
   env: {
     "jest/globals": true
