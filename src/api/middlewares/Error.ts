01ec5068997eae4f025559097e0e651eb452ec1a
r1oga <38692952+r1oga@users.noreply.github.com>
2022-10-31 17:30:42 +0100

    :loud_sound: Log errors (#7)

---
+++ src/api/middlewares/Error.ts
@@ -0,0 +1,42 @@
+import {
+  PrismaClientKnownRequestError,
+  PrismaClientValidationError,
+} from '@prisma/client/runtime'
+import * as express from 'express'
+import {
+  ExpressErrorMiddlewareInterface,
+  HttpError,
+  Middleware,
+} from 'routing-controllers'
+
+import { Logger, LoggerInterface } from '@decorators/Logger'
+import { Service } from 'typedi'
+
+@Service()
+@Middleware({ type: 'after' })
+export class ErrorHandlerMiddleware implements ExpressErrorMiddlewareInterface {
+  constructor(@Logger() private logger: LoggerInterface) {}
+
+  public error(
+    error:
+      | HttpError
+      | PrismaClientValidationError
+      | PrismaClientKnownRequestError,
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction,
+  ): void {
+    this.logger.error(
+      error instanceof PrismaClientValidationError ||
+        error instanceof PrismaClientKnownRequestError
+        ? error.constructor.name
+        : error.name,
+      process.env.NODE_ENV === 'production'
+        ? { message: error.message }
+        : {
+            message: error.message,
+            stack: error.stack,
+          },
+    )
+  }
+}
